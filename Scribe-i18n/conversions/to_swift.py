d16554048b2f670628518eb8b79e919870273a68
Jag-Marcel <marcelmehl123@gmail.com>
2024-05-30 20:48:02 +0200

    Implemented conversion from JSON to files for iOS
    Implemented a script that converts .json files to one .xcstrings file. It assumes that translated entries are already reviewed.
    todo: Needs to be an automated process, maybe automatic PRs for Scribe-iOS whenever a JSON file in Scribe-i18n changes.

---
+++ Scribe-i18n/conversions/to_swift.py
@@ -0,0 +1,54 @@
+import os
+import json
+
+directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+files = os.listdir(directory)
+langlist = sorted([file.replace('.json', '') for file in files if file.endswith('.json')])
+path = os.path.join(directory, 'en-US.json')
+file = open(path, 'r').read()
+file = json.loads(file)
+
+data = ('{\n'
+        '  "sourceLanguage" : "en",\n'
+        '  "strings" : {\n')
+pos = 0
+for key in file:
+    pos += 1
+    data += (f'    "{key}" : {{\n'
+             f'      "comment" : "{(file[key]["comment"]
+                                    .replace('"', '\\"')
+                                    .replace('\n', '\\n'))}",\n'
+             f'      "localizations" : {{\n')
+    for lang in langlist:
+        langfile = json.loads(open(os.path.join(directory, f'{lang}.json'), 'r').read())
+        if lang == 'en-US':
+            lang = 'en'
+        translation = (langfile[key]["translation"]
+                       .replace('"', '\\"')
+                       .replace('\n', '\\n'))
+        if translation == '':
+            state = ''
+        else:
+            state = 'translated'
+        data += (f'        "{lang}" : {{\n'
+                 f'          "stringUnit" : {{\n'
+                 f'            "state" : "{state}",\n'
+                 f'            "value" : "{translation}"\n'
+                 f'          }}\n'
+                 f'        }}')
+        if lang != langlist[len(langlist) - 1]:
+            data += ',\n'
+        else:
+            data += '\n'
+
+    if pos < len(file):
+        data += ('      }\n'
+                 '    },\n')
+    else:
+        data += ('      }\n'
+                 '    }\n')
+
+data += (f'  }},\n'
+         f'  "version" : "1.0"\n'
+         f'}}\n')
+open('/Users/marcelmehl/GitHub/Scribe-iOS/Scribe/Resources/Localizable.xcstrings', 'w').write(data)
