6b4392467734a77ef426d533ef009f6a2d781f73
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2024-06-13 00:37:33 +0200

    Minor formatting fixes, add pt back in and fix typo in the .yml file

--- Scribe-i18n/Scripts/convert_from_xcstrings.py
+++ Scribe-i18n/Scripts/convert_from_xcstrings.py
@@ -1,34 +1,42 @@
-import os
+"""
+Converts from an xcstrings file to JSON files.
+"""
+
 import json
+import os
 
 directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 file = open(os.path.join(directory, "Localizable.xcstrings"), "r").read()
 files = os.listdir(directory)
-langlist = [file.replace('.json', '') for file in files if file.endswith('.json')]
 
-for lang in langlist:
-    dest = open(f'{directory}/{lang}.json', 'w')
+languages = [file.replace(".json", "") for file in files if file.endswith(".json")]
+
+for lang in languages:
+    dest = open(f"{directory}/{lang}.json", "w")
     if lang == "en-US":
         lang = "en"
 
-    data = '{\n'
     json_file = json.loads(file)
     strings = json_file["strings"]
-    pos = 0
-    for key in strings:
-        pos += 1
-        translation = ''
-        if (lang in json_file["strings"][key]["localizations"]
-                and json_file["strings"][key]["localizations"][lang]["stringUnit"]["value"] != ""
-                and json_file["strings"][key]["localizations"][lang]["stringUnit"]["value"] != key):
-            translation = (json_file["strings"][key]["localizations"][lang]["stringUnit"]["value"]
-                           .replace('"', '\\"')
-                           .replace('\n', '\\n'))
+
+    data = "{\n"
+    for pos, key in enumerate(strings, start=1):
+        translation = ""
+        if (
+            lang in json_file["strings"][key]["localizations"]
+            and json_file["strings"][key]["localizations"][lang]["stringUnit"]["value"]
+            != ""
+            and json_file["strings"][key]["localizations"][lang]["stringUnit"]["value"]
+            != key
+        ):
+            translation = (
+                json_file["strings"][key]["localizations"][lang]["stringUnit"]["value"]
+                .replace('"', '\\"')
+                .replace("\n", "\\n")
+            )
         data += f'  "{key}" : "{translation}"'
-        if pos < len(json_file["strings"]):
-            data += ',\n'
-        else:
-            data += '\n'
-    data += '}\n'
+        data += ",\n" if pos < len(json_file["strings"]) else "\n"
+
+    data += "}\n"
 
     dest.write(data)
