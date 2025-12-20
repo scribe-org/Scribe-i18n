c09238db4f3cef43441b256aeaff6a230541f696
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2025-12-20 21:48:15 +0100

    Add Website i18n config file to Scribe-i18n for subtree insertion

---
+++ i18n/i18n.config.ts
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Note: This file uses AGPL-3.0-or-later as it's inserted into the scri.be code.
+export default defineI18nConfig(() => ({
+  legacy: false,
+  locale: "en",
+  fallbackLocale: "en",
+}));
